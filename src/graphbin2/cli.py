5f132462150d01aa1f46c5b007c068777f75e6b9
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2024-08-12 16:35:45 +0930

    REL: Update release number

--- src/graphbin2/cli.py
+++ src/graphbin2/cli.py
@@ -18,7 +18,7 @@ from graphbin2 import (
 __author__ = "Vijini Mallawaarachchi, Anuradha Wickramarachchi, and Yu Lin"
 __copyright__ = "Copyright 2020, GraphBin2 Project"
 __license__ = "BSD"
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 __maintainer__ = "Vijini Mallawaarachchi"
 __email__ = "viji.mallawaarachchi@gmail.com"
 __status__ = "Stable Release"
